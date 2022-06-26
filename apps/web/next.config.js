5fdf61a4f70bc6d5fc37bb6de78e0d1a61d0d159
Carlos Pavajeau <cantte098@gmail.com>
2022-06-26 10:40:14 -0500

    feat: add companies package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,5 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require('next-transpile-modules')(['@rusell-org/companies'])
 
 module.exports = withTM({
   reactStrictMode: true,
-});
+})
